9fa34a91763c08ccde86f5659de7e8ef5d0157df
Yaroslav Voloshyn <yaroslav.voloshyn@sombrainc.com>
2019-06-27 12:28:01 +0300

    test configs

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 set -xe
 
-ng test && echo "tests done !!!!!!!!!!!!!!!!!" || echo "tests failed !!!!!!!!!!!!!!!!!"
+ng test && echo "tests done !!!!!!!!!!!!!!!!!" || echo "tests failed !!!!!!!!!!!!!!!!!" && exit 0
 ng build --prod && echo "Build successes!"
